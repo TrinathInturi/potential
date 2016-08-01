232e011119008bb4af3c1261b853b805018fb265
TrinathInturi <trinathreddyinturi@gmail.com>
2016-08-01 10:33:45 +0530

    Add files via upload

---
+++ myscript2.js
@@ -0,0 +1,27 @@
+function changeImage() {
+
+        document.getElementById('myImage').src = 'bulbon.gif';
+        document.body.style.backgroundColor="skyblue";
+}
+function changeImagea() {
+
+        document.getElementById('myImage').src = 'off.gif';
+        document.body.style.backgroundColor="black";
+
+}
+
+function hideshowa(whi){
+whi.style.display="block"
+}
+
+function hideshowb(ch){
+ch.style.display="none"
+}
+
+function hidec(g){
+g.style.display="block"
+}
+
+function hided(o){
+o.style.display="none"
+}
