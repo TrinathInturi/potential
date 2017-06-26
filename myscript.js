a56fdc9dc507dad7d0cc7e13e555a908be800d80
TrinathInturi <TrinathInturi@users.noreply.github.com>
2017-06-26 19:31:32 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,169 @@
+function higha()
+{
+  document.getElementById('a').style.color="blue";
+  document.getElementById('a').style.backgroundColor="orange";
+}
+function norma()
+{
+  document.getElementById('a').style.color="white";
+  document.getElementById('a').style.backgroundColor="#4CAF50";
+}
+
+
+function highb()
+{
+  document.getElementById('b').style.color="blue";
+  document.getElementById('b').style.backgroundColor="orange";
+}
+function normb()
+{
+  document.getElementById('b').style.color="white";
+  document.getElementById('b').style.backgroundColor="#4CAF50";
+}
+
+
+function highc()
+{
+  document.getElementById('c').style.color="blue";
+  document.getElementById('c').style.backgroundColor="orange";
+}
+function normc()
+{
+  document.getElementById('c').style.color="white";
+  document.getElementById('c').style.backgroundColor="#4CAF50";
+}
+
+
+function highd()
+{
+  document.getElementById('d').style.color="blue";
+  document.getElementById('d').style.backgroundColor="orange";
+}
+function normd()
+{
+  document.getElementById('d').style.color="white";
+  document.getElementById('d').style.backgroundColor="#4CAF50";
+}
+
+
+function highe()
+{
+  document.getElementById('e').style.color="blue";
+  document.getElementById('e').style.backgroundColor="orange";
+}
+function norme()
+{
+  document.getElementById('e').style.color="white";
+  document.getElementById('e').style.backgroundColor="#4CAF50";
+}
+function highf()
+{
+  document.getElementById('f').style.color="blue";
+  document.getElementById('f').style.backgroundColor="orange";
+
+}
+function normf()
+{
+  document.getElementById('f').style.color="white";
+  document.getElementById('f').style.backgroundColor="#4CAF50";
+}
+function highg()
+{
+  document.getElementById('g').style.color="blue";
+  document.getElementById('g').style.backgroundColor="orange";
+}
+function normg()
+{
+  document.getElementById('g').style.color="white";
+  document.getElementById('g').style.backgroundColor="#4CAF50";
+}
+function highh()
+{
+  document.getElementById('h').style.color="blue";
+  document.getElementById('h').style.backgroundColor="orange";
+}
+function normh()
+{
+  document.getElementById('h').style.color="white";
+  document.getElementById('h').style.backgroundColor="#4CAF50";
+}
+function highi()
+{
+  document.getElementById('i').style.color="blue";
+  document.getElementById('i').style.backgroundColor="orange";
+}
+function normi()
+{
+  document.getElementById('i').style.color="white";
+  document.getElementById('i').style.backgroundColor="#4CAF50";
+}
+function highj()
+{
+  document.getElementById('j').style.color="blue";
+  document.getElementById('j').style.backgroundColor="orange";
+}
+function normj()
+{
+  document.getElementById('j').style.color="white";
+  document.getElementById('j').style.backgroundColor="#4CAF50";
+}
+function highk()
+{
+  document.getElementById('k').style.color="blue";
+  document.getElementById('k').style.backgroundColor="orange";
+}
+function normk()
+{
+  document.getElementById('k').style.color="white";
+  document.getElementById('k').style.backgroundColor="#4CAF50";
+}
+function highl()
+{
+  document.getElementById('l').style.color="blue";
+  document.getElementById('l').style.backgroundColor="orange";
+}
+function norml()
+{
+  document.getElementById('l').style.color="white";
+  document.getElementById('l').style.backgroundColor="#4CAF50";
+}
+function highm()
+{
+  document.getElementById('m').style.color="blue";
+  document.getElementById('m').style.backgroundColor="orange";
+}
+function normm()
+{
+  document.getElementById('m').style.color="white";
+  document.getElementById('m').style.backgroundColor="#4CAF50";
+}
+function highn()
+{
+  document.getElementById('n').style.color="blue";
+  document.getElementById('n').style.backgroundColor="orange";
+}
+function normn()
+{
+  document.getElementById('n').style.color="white";
+  document.getElementById('n').style.backgroundColor="#4CAF50";
+}
+function higho()
+{
+  document.getElementById('o').style.color="blue";
+  document.getElementById('o').style.backgroundColor="orange";
+}
+function normo()
+{
+  document.getElementById('o').style.color="white";
+  document.getElementById('o').style.backgroundColor="#4CAF50";
+}
+function highp()
+{
+  document.getElementById('p').style.color="blue";
+  document.getElementById('p').style.backgroundColor="orange";
+}
+function normp()
+{
+  document.getElementById('p').style.color="white";
+  document.getElementById('p').style.backgroundColor="#4CAF50";
+}
